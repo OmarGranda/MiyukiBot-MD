a6ce768d993a76913b157f53950095ae2b668e89
OmarGranda <omargranda673@gmail.com>
2025-10-29 08:42:59 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -31,7 +31,7 @@ try {
     if (!m) return
     if (!m.isGroup) {
         const text = (m.text || '').trim().toLowerCase()
-        if (text !== '') return
+        if (text !== '.ping') return
     }
 
 m.exp = 0
