249a4fcd1fb87eb1f28d74446b34c6a44e2a4333
OmarGranda <omargranda673@gmail.com>
2025-11-04 23:14:36 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -332,6 +332,10 @@ __filename
 }))
 continue
 }
+if (typeof global.plugins !== 'object' || !global.plugins) {
+  global.plugins = {}
+  console.log('[AutoFix] Se inicializó global.plugins porque estaba undefined.')
+}
 if (typeof plugin !== 'function')
 continue
 if ((usedPrefix = (match[0] || '')[0])) {
