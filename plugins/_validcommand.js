d122487e3351c98bdfe3c1c52b0af11e24d059ff
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-11-17 08:50:05 -0500

    Actualizar _validcommand.js

--- plugins/_validcommand.js
+++ plugins/_validcommand.js
@@ -6,7 +6,7 @@ export async function before(m, { conn }) {
   const usedPrefix = global.prefix.exec(m.text)[0];
   const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
 
-  const thumbRes = await fetch("https://qu.ax/hHhch.jpg");
+  const thumbRes = await fetch("https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1763387370316_434732.jpeg");
   const thumbBuffer = await thumbRes.buffer();
 
   const fkontak = {
